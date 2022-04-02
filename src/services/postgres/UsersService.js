88086c07da63a11902594f5922868cc902c67e04
muhajir.shiddiq <muhajir.shiddiq@id.fujitsu.internal>
2022-04-02 17:30:22 +0700

    Add authentication token for notes apps

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const { mapDBToModel } = require('../../utils');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
+
 
 class UsersService {
   constructor() {
@@ -58,6 +60,28 @@ class UsersService {
     }
  
     return result.rows[0];
+  }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+ 
+    const result = await this._pool.query(query);
+ 
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+ 
+    const { id, password: hashedPassword } = result.rows[0];
+ 
+    const match = await bcrypt.compare(password, hashedPassword);
+ 
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+    return id;
   }
 
 }
